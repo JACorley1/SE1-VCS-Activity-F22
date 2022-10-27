dc562e19fdbbba6b8015eca4687c4c9abbd0f11f
ljones48 <ljones48@my.westga.edu>
2022-10-27 13:41:23 -0400

    Add getters and setters for private members in Dragon class.
    Add fly method to Dragon class.

--- code/Activity 3 - VCS/src/edu/westga/cs3211/a3/model/Dragon.java
+++ code/Activity 3 - VCS/src/edu/westga/cs3211/a3/model/Dragon.java
@@ -5,12 +5,41 @@ public class Dragon {
 	private int wingSpan;
 	private boolean breathesFire;
 	
+	public final int MIN_WING_SPAN = 10;
+	
 	public Dragon(int wingSpan, boolean breathesFire) {
 		if (wingSpan <= 0 ) {
 			throw new IllegalArgumentException("Wing Span cannot be less than 1.");
 		}
 		this.wingSpan = wingSpan;
 		this.breathesFire = breathesFire;
+	}
+	
+	public int getWingSpan() {
+		return this.wingSpan;
+	}
+
+	public void setWingSpan(int wingSpan) {
+		this.wingSpan = wingSpan;
+	}
+
+	public boolean isBreathesFire() {
+		return this.breathesFire;
+	}
+
+	public void setBreathesFire(boolean breathesFire) {
+		this.breathesFire = breathesFire;
+	}
+	
+	public boolean fly() {
+		boolean result;
+		if (this.wingSpan >= this.MIN_WING_SPAN) {
+			result = true;
+		}else {
+			result = false;
+		}
+		
+		return result;
 	}
 
 }
