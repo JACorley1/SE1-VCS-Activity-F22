a206d733ce664b1a11ee11d52fce7cfa544725a2
TurtleHermit12 <110193072+TurtleHermit12@users.noreply.github.com>
2022-10-27 13:29:42 -0400

    Add datamembers wingSpan and breathesFire to Dragon Class
    Add constructor to Dragon class

--- code/Activity 3 - VCS/src/edu/westga/cs3211/a3/model/Dragon.java
+++ code/Activity 3 - VCS/src/edu/westga/cs3211/a3/model/Dragon.java
@@ -1,5 +1,16 @@
 package edu.westga.cs3211.a3.model;
 
 public class Dragon {
+	
+	private int wingSpan;
+	private boolean breathesFire;
+	
+	public Dragon(int wingSpan, boolean breathesFire) {
+		if (wingSpan <= 0 ) {
+			throw new IllegalArgumentException("Wing Span cannot be less than 1.");
+		}
+		this.wingSpan = wingSpan;
+		this.breathesFire = breathesFire;
+	}
 
 }
