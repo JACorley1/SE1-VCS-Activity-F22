512008c2fab0e7c0df38ed67fa6de30524f95a16
Jonathan Corley <jcorley@westga.edu>
2022-10-27 09:20:11 -0400

    Adding Base Project

---
+++ code/Activity 3 - VCS/src/edu/westga/cs3211/a3/model/Monkey.java
@@ -0,0 +1,5 @@
+package edu.westga.cs3211.a3.model;
+
+public class Monkey {
+
+}
---
+++ code/Activity 3 - VCS/src/edu/westga/cs3211/a3/test/goat/TestCosntructor.java
@@ -0,0 +1,14 @@
+package edu.westga.cs3211.a3.test.goat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCosntructor {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
