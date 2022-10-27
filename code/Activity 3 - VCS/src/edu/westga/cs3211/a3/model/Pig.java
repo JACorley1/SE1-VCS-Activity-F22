d28200a44b67501370a5e31959e5f012fe1ff26e
csuser@cs.westga.edu <csuser@cs.westga.edu>
2022-10-27 13:40:38 -0400

    add color and age to pig class

--- code/Activity 3 - VCS/src/edu/westga/cs3211/a3/model/Pig.java
+++ code/Activity 3 - VCS/src/edu/westga/cs3211/a3/model/Pig.java
@@ -1,5 +1,41 @@
 package edu.westga.cs3211.a3.model;
 
 public class Pig {
+	
+	private String color;
+	
+	private int age;
+	
+	
+	public Pig(String color, int age) {
+		this.setColor(color);
+		this.setAge(age);
+	}
+
+
+	public String getColor() {
+		return color;
+	}
+
+
+	public void setColor(String color) {
+		if(color.isEmpty()) {
+			throw new IllegalArgumentException("Color cannot be blank");
+		}
+		this.color = color;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		if(age < 0 || age > 23) {
+			throw new IllegalArgumentException();
+		}
+		this.age = age;
+	}
 
 }
