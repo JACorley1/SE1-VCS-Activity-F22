74c87c848dc5540d615da01886d4f7eeb5f343b2
Steven Kight <sgk0711@gmail.com>
2022-10-27 13:41:19 -0400

    Implemented and Tested Ox Class

--- code/Activity 3 - VCS/src/edu/westga/cs3211/a3/model/Ox.java
+++ code/Activity 3 - VCS/src/edu/westga/cs3211/a3/model/Ox.java
@@ -1,5 +1,26 @@
 package edu.westga.cs3211.a3.model;
 
 public class Ox {
+	private int year;
+	private String favoriteFood;
+	
+	public int getYear() {
+		return year;
+	}
 
+	public String getFavoriteFood() {
+		return favoriteFood;
+	}
+
+	public Ox(int year, String favoriteFood) {
+		if (favoriteFood.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+		if (year <= 0) {
+			throw new IllegalArgumentException();
+		}
+		
+		this.year = year;
+		this.favoriteFood = favoriteFood;
+	}
 }
